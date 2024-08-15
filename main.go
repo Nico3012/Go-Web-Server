f09fdb3cf76c5094ecae9f9cf186f36b5526c16d
Nico Hörter <nico.m.hoerter@gmail.com>
2024-08-15 15:27:57 +0200

    start with manual headers, status responses etc.

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	handler := http.NewServeMux()
+
+	/*handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
+	})*/
+
+	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "<h1>Hallo Welt!</h1>")
 	})
 
-	log.Fatal(http.ListenAndServeTLS(":4433", "tls/cert/trusted/cert.pem", "tls/cert/trusted/priv.pem", nil))
+	server := &http.Server{
+		Addr:    ":4433",
+		Handler: handler,
+	}
+
+	server.ListenAndServeTLS("tls/cert/trusted/cert.pem", "tls/cert/trusted/priv.pem")
 }
