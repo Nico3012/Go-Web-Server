3e0ac3475a5cf8bcb66faefce5312d8c0dce1bb1
Nico Hörter <nico.m.hoerter@gmail.com>
2024-08-16 15:45:28 +0200

    start reading files self programmed and bring a order to w.Header, w.WriteHeader, fmt.Fprintf

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,9 +14,21 @@ func main() {
 	})*/
 
 	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/html; charset=utf-8")
-		w.WriteHeader(400)
-		fmt.Fprintf(w, "<h1>Hallo Welt!!!</h1>")
+		if r.Method == "GET" {
+			url := r.URL.String()
+			file, err := os.ReadFile("app" + url)
+			if err != nil {
+				fmt.Fprintf(w, "Failed to read file!")
+			}
+			w.Header().Add("Content-Type", "text/html; charset=utf-8") // first write headers
+			w.WriteHeader(200)                                         // send headers with status
+			fmt.Fprintf(w, string(file))                               // last send content
+		} else {
+			fmt.Fprintf(w, "This server only supports GET method! You sent a %s request.", r.Method)
+		}
+		//w.Header().Add("Content-Type", "text/html; charset=utf-8")
+		//w.WriteHeader(400)
+		//fmt.Fprintf(w, "<h1>Hallo Welt!!!</h1>")
 	})
 
 	server := &http.Server{
@@ -23,5 +36,5 @@ func main() {
 		Handler: handler,
 	}
 
-	server.ListenAndServeTLS("tls/cert-rsa/trusted/cert.pem", "tls/cert-rsa/trusted/priv.pem")
+	server.ListenAndServeTLS("tls/cert-ecdsa/trusted/cert.pem", "tls/cert-ecdsa/trusted/priv.pem")
 }
