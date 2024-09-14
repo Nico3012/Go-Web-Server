b56c887f27397521b270ac1cc77244501f5887ac
Nico Hörter <nico.m.hoerter@gmail.com>
2024-09-14 18:35:07 +0200

    update to use github.com/Nico3012/webserver package for webserver

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Nico3012/Go-Web-Server
 
-go 1.23.0
+go 1.23.1
+
+require github.com/Nico3012/webserver v0.1.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/x509/pkix"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,36 +10,8 @@ import (
 	"os"
 
 	"github.com/Nico3012/Go-Web-Server/push"
-	"github.com/Nico3012/Go-Web-Server/webserver"
+	"github.com/Nico3012/webserver"
 )
-
-// func getContentType(filename string) string {
-// 	if strings.HasSuffix(filename, ".txt") {
-// 		return "text/plain; charset=utf-8"
-// 	}
-// 	if strings.HasSuffix(filename, ".html") {
-// 		return "text/html; charset=utf-8"
-// 	}
-// 	if strings.HasSuffix(filename, ".css") {
-// 		return "text/css; charset=utf-8"
-// 	}
-// 	if strings.HasSuffix(filename, ".js") {
-// 		return "text/javascript; charset=utf-8"
-// 	}
-// 	if strings.HasSuffix(filename, ".json") {
-// 		return "application/json; charset=utf-8"
-// 	}
-// 	if strings.HasSuffix(filename, ".webmanifest") {
-// 		return "application/manifest+json; charset=utf-8"
-// 	}
-// 	if strings.HasSuffix(filename, ".wasm") {
-// 		return "application/wasm"
-// 	}
-// 	if strings.HasSuffix(filename, ".png") {
-// 		return "image/png"
-// 	}
-// 	return "application/octet-stream"
-// }
 
 type PushConfig struct {
 	PublicKey     string
@@ -125,45 +96,14 @@ func main() {
 		fs.ServeHTTP(w, r)
 	})
 
-	// handler.Handle("/", http.FileServer(http.Dir("app")))
-
-	// handler := http.NewServeMux()
-
-	// handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	if r.Method == "GET" {
-	// 		html, err := os.ReadFile("app" + r.URL.String())
-	// 		if err != nil {
-	// 			return
-	// 		}
-
-	// 		w.Header().Set("Content-Type", getContentType(r.URL.String()))
-	// 		w.WriteHeader(200)
-	// 		w.Write(html)
-	// 	} else {
-	// 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	// 		// "Content-Length" and "Date" header will be added automatically
-	// 		w.WriteHeader(500)
-	// 		w.Write([]byte("Only GET requests are supported"))
-	// 	}
-	// })
-
-	err := webserver.CreateWebServerAndCertificate(":443", "tls/ca/trusted/ca.pem", "tls/ca/trusted/key.pem", pkix.Name{
-		// if this information is missing, the certificate may not be trusted:
-		CommonName:         "liquipay.de",                                // required by openssl
-		Organization:       []string{"Liquipay UG (haftungsbeschränkt)"}, // required by openssl
-		OrganizationalUnit: []string{"IT"},                               // required by openssl
-		Country:            []string{"DE"},                               // required by openssl
-		Province:           []string{"Nordrhein-Westfalen"},              // required by openssl
-		Locality:           []string{"Lindlar"},                          // required by openssl
-		PostalCode:         []string{"51789"},                            // optional
-		StreetAddress:      []string{"Hauptstraße 10"},                   // optional
-	}, []string{
+	err := webserver.CreateWebServerAndCertificate(":443", "ca.pem", "key.pem", []string{
 		"localhost",
 		"127.0.0.1",
 		"192.168.2.53",
 		"192.168.2.79",             //
-		"nico-laptop.speedport.ip", // Nico-Laptop Speedport
-		"192.168.2.225",            // Noah router
+		"nico-laptop.speedport.ip", // Nico-Laptop @ any Speedport Router
+		"nico-laptop.fritz.box",    // Nico-Laptop @ any FritzBox Router
+		"192.168.2.225",            // Noah Speedport Router
 	}, handler)
 
 	log.Fatalf("Failed to create web server and certificate: %v", err)
